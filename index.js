703f372966bbd384a21484c81ea89c0801b1b230
CorinneKelly <corinnemariekelly@gmail.com>
2017-04-01 11:57:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 //Code React element here
 const meInReact = React.createElement('div', {className: 'me'},
-	React.createElement('h1', {}, 'An Awesome Person'),
-	React.createElement('p', {}, 'Who is learning React'),
+	React.createElement('h1', {}, 'Mad Wicked Programmer'),
+	React.createElement('p', {}, 'Who is dope AF'),
 	React.createElement('ul', {className: 'me__interests'},
 		[
-			React.createElement('li', {}, 'JavaScript'),
+			React.createElement('li', {}, 'Aspiring Surfer'),
 			React.createElement('li', {}, 'Hot Dad Enthusiast'),
 			React.createElement('li', {}, 'Adventurer'),
 			React.createElement('li', {}, 'Traveler')
